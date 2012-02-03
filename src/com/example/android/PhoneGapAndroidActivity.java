c848ce0b1d9ea00c871d97a75e43992dc3c5586c
Salvador Mora <chavastol@gmail.com>
2012-02-03 10:55:16 -0700

    commit all

---
+++ src/com/example/android/PhoneGapAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.example.android;
+
+import android.os.Bundle;
+import com.phonegap.DroidGap;
+
+public class PhoneGapAndroidActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
